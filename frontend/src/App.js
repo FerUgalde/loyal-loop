6390767611d822c44aaf7f682dd6c95958b08f1b
Fernanda Ugalde <fernanda.ugalde@uabc.edu.mx>
2025-07-14 16:42:26 +0200

    Wallet connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import WalletConnector from './components/WalletConnector';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <img src={""} className="App-logo" alt="LoyalLoop" />
+      <h1>LoyalLoop</h1>
+      <WalletConnector />
+      <p>
+        Welcome to LoyalLoop, your gateway to seamless loyalty rewards.
+      </p>
     </div>
   );
 }
